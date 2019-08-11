22db212e1c12c98cbc6ba498ddb6170d13d1d79d
luyinjian <luyinjian@outlook.com>
2019-08-11 12:23:59 +0800

    WWW

---
+++ ReportCreater/FileHandler/DailySendInfoHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportCreater.FileHandler
+{
+    public class DailySendInfoHandler
+    {
+        public DailySendInfoHandler(string filePath,DateTime date)
+        {
+            
+        }
+    }
+}
